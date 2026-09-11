8a0addae4ac9d4b48b453632f55b472887a272a4
Oleg Moshenskiy <o.moshenskyi@ukma.edu.ua>
2026-09-11 15:36:49 +0300

    Create DeviceSimulatorApplication class for Spring Boot

---
+++ device-simulator/src / main / java / simulator /DeviceSimulatorApplication.java
@@ -0,0 +1,11 @@
+package simulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeviceSimulatorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DeviceSimulatorApplication.class, args);
+    }
+}
