4e42c5d242d817df8437315b72fa6399303ed116
Oleg Moshenskiy <o.moshenskyi@ukma.edu.ua>
2026-09-11 15:25:38 +0300

    Initialize Spring Boot application in mainFile.java

---
+++ client-console/src/main/java/mainFile/mainFile.java
@@ -0,0 +1,6 @@
+@SpringBootApplication
+public class ClientConsoleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ClientConsoleApplication.class, args);
+    }
+}
