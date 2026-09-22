835ac6b7b3454654b9be571ea125f3de18961230
Oleg Moshenskiy <o.moshenskyi@ukma.edu.ua>
2026-09-22 14:45:04 +0300

    Add static factory methods for ExecutionOutcome
    Added static methods for creating ExecutionOutcome instances.

--- server/src/main/java/server/executions/ExecutionOutcome.java
+++ server/src/main/java/server/executions/ExecutionOutcome.java
@@ -1,11 +1,21 @@
-package server.executions;
-
 public record ExecutionOutcome(Status status, String message) {
 
     public enum Status {
         SUCCESS,
         REJECTED,
         DEVICE_UNAVAILABLE
+    }
+
+    public static ExecutionOutcome success(String message) {
+        return new ExecutionOutcome(Status.SUCCESS, message);
+    }
+
+    public static ExecutionOutcome rejected(String message) {
+        return new ExecutionOutcome(Status.REJECTED, message);
+    }
+
+    public static ExecutionOutcome deviceUnavailable(String message) {
+        return new ExecutionOutcome(Status.DEVICE_UNAVAILABLE, message);
     }
 
     public boolean isSuccess() {
